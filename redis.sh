1c00c3ff5af403ed68897bdafb65cca149ae792b
sefooking <sefoo510@gmail.com>
2016-08-17 10:48:56 +0300

    Add files via upload

---
+++ redis.sh
@@ -0,0 +1,23 @@
+redis-server
+
+cd king-bot
+./launch.sh
+redis-server
+cd king-bot
+./launch.sh
+redis-server
+cd king-bot
+./launch.sh
+redis-server
+cd king-bot
+./launch.sh
+redis-server
+cd king-bot
+./launch.sh
+redis-server
+cd king-bot
+./launch.sh
+redis-server
+cd king-bot
+./launch.sh
+redis-server
